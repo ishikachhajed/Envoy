77bba8c9ff330707e42628ab42a1b3c68c7cb0f5
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-05-20 13:59:57 +0530

    Corrected CreateSecretRequestDTO.java

--- backend/src/main/java/com/example/backend/dto/CreateSecretRequestDTO.java
+++ backend/src/main/java/com/example/backend/dto/CreateSecretRequestDTO.java
@@ -21,6 +21,12 @@ public class CreateSecretRequestDTO {
     public String getKey() {
         return key;
     }
+    public void setKey(String key) {
+        this.key = key;
+    }
+    public String getValue() {
+        return value;
+    }
     public void setValue(String value) {
         this.value = value;
     }
