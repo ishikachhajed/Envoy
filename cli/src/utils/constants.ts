5b958a924b8051ac1cde1bc933345ad2435d9a54
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-05-25 23:50:07 +0530

    Add CLI Constants And Configuration Paths

---
+++ cli/src/utils/constants.ts
@@ -0,0 +1,7 @@
+import os from 'os';
+import path from 'path';
+
+export const CONFIG_DIR = path.join(os.homedir(), '.envoy');
+export const CONFIG_FILE_PATH = path.join(CONFIG_DIR, 'config.json');
+
+export const DEFAULT_BACKEND_URL = 'http://localhost:8080';
