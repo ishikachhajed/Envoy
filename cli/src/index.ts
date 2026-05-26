3a3f4d28de2b70990de638a7b2a8c4dd821b4b79
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-05-26 11:05:20 +0530

    Register Organization And Project Commands In CLI

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -2,6 +2,8 @@ import { Command } from 'commander';
 import { logger } from './utils/logger.js';
 import { logoutCommand } from './commands/logout.js';
 import { loginCommand } from './commands/login.js';
+import { orgCommand } from './commands/organization.js';
+import { projectCommand } from './commands/project.js';
 
 const program = new Command();
 
@@ -12,6 +14,8 @@ program
 
 program.addCommand(loginCommand);
 program.addCommand(logoutCommand);
+program.addCommand(orgCommand);
+program.addCommand(projectCommand);
 
 if (!process.argv.slice(2).length) {
   program.outputHelp();
